699aa27c8ed5ec1a8c59a48cd467c768dc2a24ae
Mizanur Rahman <mdmizanurrahmanhridoy5@gmail.com>
2026-07-11 23:11:02 +0600

    remove css code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
-
 function App() {
   return (
     <div>
-      <h2>Plant Net</h2>
+      <h2>Plant Net....</h2>
     </div>
   );
 }
