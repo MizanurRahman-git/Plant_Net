b6fc4437a1e47fbc94ce15bfe3a01d5433ea87f2
Mizanur Rahman <mdmizanurrahmanhridoy5@gmail.com>
2026-07-18 18:54:18 +0600

    added router management

--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../layouts/MainLayout";
+import ErrorPage from "../pages/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "/",
+        element: <div>Home Section</div>
+      }
+    ]
   },
 ]);
